b6e7b4ded170347e5cb1a2bdeaf24610cf9ef81b
DatlaRakeshVarma <datla.rakeshvarma@gmsil.com>
2025-09-03 13:36:22 +0530

    blog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css'
 import Home from './pages/Home';
+import BlogPage from './pages/BlogPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+  // Global scroll restoration prevention
+  useEffect(() => {
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual';
+    }
+  }, []);
 
   return (
-    <>
-      <div>
-        <Home />
-      </div>
-        
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<BlogPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
